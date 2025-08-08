2e618fd5495526d8b113487af8b383c579129af6
tihomirtx88 <tihomirtx88@gmail.com>
2025-08-08 15:14:54 +0300

    Refactor
    Refactor main.py

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 from flask import Flask, render_template
 
-app = Flask("Whater");
+app = Flask(__name__);
 
 @app.route("/")
 def home():
     return render_template("index.html");
 
-@app.route("/about")
-def about():
-    return render_template("about.html");
-
-app.run(debug=True);
+@app.route("/api/v1/<station>/<date>")
+def about(station, date):
+    temperature = 23;
+    return {
+        "station": station,
+        "date": date,
+        "temperature": temperature
+    };
+if __name__ == "__main__":
+  app.run(debug=True);
